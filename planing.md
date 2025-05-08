835802ed4a86eee8a922e11b0f275a16235d0b82
raxitsanghani <raxitsanghani@gmail.com>
2025-05-08 18:14:27 +0530

    app update

---
+++ planing.md
@@ -0,0 +1,4 @@
+- When another user replies, the message should appear aligned accordingly.
+
+- The chat should auto-scroll to the newest message.
+- A user should only be able to delete their own messages.
--- src/server.js
+++ src/server.js
@@ -53,6 +53,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000...");
+server.listen(8080, () => {
+  console.log("Server is successfully listening on port 8080...");
 });
